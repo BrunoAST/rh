a8f35379a4a3dccad62c312fb08716d294ad499e
Bruno Alves <bruno-alves2010@live.com>
2021-12-02 21:56:13 -0300

    chore: add type lcov to karma reporter

--- karma.conf.js
+++ karma.conf.js
@@ -3,14 +3,14 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma")
     ],
     client: {
       jasmine: {
@@ -25,19 +25,18 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/rh'),
-      subdir: '.',
+      dir: require("path").join(__dirname, "./coverage/rh"),
+      subdir: ".",
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        { type: "lcov" }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false,
     restartOnFileChange: true
   });
