261dbee33ad9ae3b4b9449547736ed51ea7247b4
Bruno Alves <bruno-alves2010@live.com>
2021-12-16 20:31:04 -0300

    test: add tests for validity methods

--- src/app/shared/contracts/base-form-service/test/base-form-service-stub.ts
+++ src/app/shared/contracts/base-form-service/test/base-form-service-stub.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
+import { Validators } from "@angular/forms";
+
 import { BaseFormService } from "../base-form-service";
 
 @Injectable()
 export class BaseFormServiceStub extends BaseFormService {
   buildForm(): void {
     this.form = this.formBuilder.group({
-      field: [""]
+      field: ["", [Validators.required]]
     });
   }
 }
--- src/app/shared/contracts/base-form-service/test/base-form-service.spec.ts
+++ src/app/shared/contracts/base-form-service/test/base-form-service.spec.ts
@@ -43,10 +43,30 @@ describe(BaseFormService.name, () => {
     expect(baseFormService.getFieldValue("field")).toBe(value);
   });
 
-  it(`#${BaseFormService.prototype.getFieldValue.name} should throw when informed field doesn't exist in the form`, () => {
-    const fakeField = faker.random.alphaNumeric();
-    expect(() => baseFormService.getFieldValue(fakeField))
-      .withContext(`Field not found: ${fakeField}`)
-      .toThrow(new Error(`The field ${fakeField} doesn't exist in the form`));
+  it(`#${BaseFormService.prototype.isFormInvalid.name} should return true when form invalid`, () => {
+    expect(baseFormService.isFormInvalid()).toBeTruthy();
+  });
+
+  it(`#${BaseFormService.prototype.isFormInvalid.name} should return false when form valid`, () => {
+    baseFormService.updateFieldValue("field", faker.random.word());
+    expect(baseFormService.isFormInvalid()).toBeFalsy();
+  });
+
+  it(`#${BaseFormService.prototype.isFieldInvalid.name} should return true when field invalid`, () => {
+    expect(baseFormService.isFieldInvalid("field")).toBeTruthy();
+  });
+
+  it(`#${BaseFormService.prototype.isFieldInvalid.name} should return false when field valid`, () => {
+    baseFormService.updateFieldValue("field", faker.random.word());
+    expect(baseFormService.isFieldInvalid("field")).toBeFalsy();
+  });
+
+  it(`#${BaseFormService.prototype.hasError.name} should return true when field contains an error`, () => {
+    expect(baseFormService.hasError("field", "required")).toBeTruthy();
+  });
+
+  it(`#${BaseFormService.prototype.hasError.name} should return false when field doesn't contain errors`, () => {
+    baseFormService.updateFieldValue("field", faker.random.word());
+    expect(baseFormService.hasError("field", "required")).toBeFalsy();
   });
 });
