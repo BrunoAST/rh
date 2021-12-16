f2d09ebda5c72613c64c0c45e6c6071222afbf9e
Bruno Alves <bruno-alves2010@live.com>
2021-12-16 20:35:03 -0300

    feat: add main container routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./main-container/main-container.module").then(m => m.MainContainerModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
