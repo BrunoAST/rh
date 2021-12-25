b1925bb89c23ef222b1a751e2d6d6a324102d9c6
Bruno Alves <bruno-alves2010@live.com>
2021-12-24 23:47:46 -0300

    test: add faker to mock urls

--- src/app/shared/layout/side-bar-container/test/menu-items-mock.ts
+++ src/app/shared/layout/side-bar-container/test/menu-items-mock.ts
@@ -1,16 +1,15 @@
 import { MenuItems } from "@innove/rh-core-ui";
 import * as faker from "faker";
-import { APP_ROUTES } from "src/app/shared/routes/app-routes";
 
 export const MENU_ITEMS_MOCK: MenuItems[] = [
   {
     name: faker.random.word(),
-    url: APP_ROUTES.competenceRegister,
+    url: faker.internet.url(),
     ionIconName: faker.random.word()
   },
   {
     name: faker.random.word(),
-    url: "/",
+    url: faker.internet.url(),
     ionIconName: faker.random.word()
   }
 ];
