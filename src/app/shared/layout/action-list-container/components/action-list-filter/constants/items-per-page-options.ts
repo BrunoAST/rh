c554285a0d47e221fc48379eac3d8811b1a06421
BrunoAST <bruno-alves2010@live.com>
2022-01-29 19:52:22 -0300

    refactor: create type to items-per-page-options

---
+++ src/app/shared/layout/action-list-container/components/action-list-filter/constants/items-per-page-options.ts
@@ -0,0 +1,20 @@
+import { SelectOptions } from "@innove/rh-core-ui/dist/types/components/select/types/select-options";
+
+export const ITEMS_PER_PAGE_OPTIONS: SelectOptions[] = [
+  {
+    title: "10",
+    value: "10"
+  },
+  {
+    title: "20",
+    value: "20"
+  },
+  {
+    title: "30",
+    value: "30"
+  },
+  {
+    title: "50",
+    value: "50"
+  }
+];
