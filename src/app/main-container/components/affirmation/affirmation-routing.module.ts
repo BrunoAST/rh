a9d3dd0ff0d7004062f3626cf1b2bbd2fa40f406
BrunoAST <bruno-alves2010@live.com>
2022-02-05 14:52:47 -0300

    feat: add route for affirmation register component

--- src/app/main-container/components/affirmation/affirmation-routing.module.ts
+++ src/app/main-container/components/affirmation/affirmation-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-// import { APP_ROUTES } from "../../../shared/routes/app-routes";
+import { APP_ROUTES } from "../../../shared/routes/app-routes";
 
 const ROUTES: Routes = [
   {
@@ -9,10 +9,10 @@ const ROUTES: Routes = [
     pathMatch: "full",
     loadChildren: () => import("./components/affirmation-list/affirmation-list.module").then(m => m.AffirmationListModule)
   },
-  // {
-  //   path: APP_ROUTES.register,
-  //   loadChildren: () => import("./components/affirmation-register/affirmation-register.module").then(m => m.AffirmationRegisterModule)
-  // }
+  {
+    path: APP_ROUTES.register,
+    loadChildren: () => import("./components/affirmation-register/affirmation-register.module").then(m => m.AffirmationRegisterModule)
+  }
 ];
 
 @NgModule({
