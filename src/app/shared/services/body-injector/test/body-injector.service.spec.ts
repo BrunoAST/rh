f46a5c5c22da916db4f5f07a7f41381c6aaeb90e
Bruno Alves <bruno-alves2010@live.com>
2022-02-17 21:56:38 -0300

    test: create tests for body injector service

---
+++ src/app/shared/services/body-injector/test/body-injector.service.spec.ts
@@ -0,0 +1,32 @@
+import { ApplicationRef, Component } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { BodyInjectorService } from "src/app/shared/services/body-injector/body-injector.service";
+
+@Component({
+  selector: 'app-mock-component',
+  template: ''
+})
+class MockComponent { }
+
+let service: BodyInjectorService;
+let fixture: ComponentFixture<MockComponent>;
+let applicationRef: ApplicationRef;
+
+describe(BodyInjectorService.name, () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [MockComponent],
+    }).compileComponents();
+    service = TestBed.inject(BodyInjectorService);
+    applicationRef = TestBed.inject(ApplicationRef);
+    fixture = TestBed.createComponent(MockComponent);
+  });
+
+  it("Should attach component", () => {
+    service.stackBeforeAppRoot(fixture.componentRef);
+    console.log(fixture.nativeElement)
+    expect(applicationRef.viewCount).toBe(1);
+  });
+});
+
